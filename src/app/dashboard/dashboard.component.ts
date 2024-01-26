47b0ab17bdbe396ac58826badc4e515b754cd8de
STARS1LVER <diegocardenaz11@gmail.com>
2024-01-26 17:42:12 -0500

    agregando tailwinds

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SideMenuComponent } from '../shared/side-menu/side-menu.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule ],
+  imports: [CommonModule, RouterModule, SideMenuComponent ],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
-export class DashboardComponent {
+export default class DashboardComponent {
 
 }
--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
+
+  // Esta propiedad contiene todo acerca de las rutas y nos permite ser un poco mas dinamico con el menu
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat() // aplanamos el arreglo, osea si el arreglo es bidimensional ahora sera unidimencional
+    .filter( route => route && route.path) // filtramos para que solo este las rutas que tienen path
+    .filter( route => !route.path?.includes(':')) // escluimos las rutas que tienen 2 puntos
+
+  constructor(){
+
+  }
 
 }
