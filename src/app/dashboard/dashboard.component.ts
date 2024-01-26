9e7c21dc3a1a41df82c227fca27fbba07abf51d0
STARS1LVER <diegocardenaz11@gmail.com>
2024-01-26 17:50:01 -0500

    Agregando alias

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SideMenuComponent } from '../shared/side-menu/side-menu.component';
+import { SideMenuComponent } from '@shared/side-menu/side-menu.component'; // el alias @shared se creo desde tsconfig
+
 
 @Component({
   standalone: true,
