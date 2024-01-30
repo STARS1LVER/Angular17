aa2db1c6ef836e56e6d98d0155bfeac76509f237
STARS1LVER <diegocardenaz11@gmail.com>
2024-01-30 18:22:41 -0500

    Aplicando las nuevas funciones

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+type Grade = 'A'|'B'|'C'; // Hacemos este tipado para controlar el valor
+
 
 @Component({
   standalone: true,
@@ -7,5 +10,15 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export default class ControlFlowComponent {
+
+  // Creamos una señal
+  public showContent = signal(false);
+
+  public grade = signal<Grade>('C');
+
+
+  public toggleContent(){
+    this.showContent.update( value => !value) // Hacemos la negacion del valor
+  }
 
 }
