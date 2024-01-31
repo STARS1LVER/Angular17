3303d6a4889d2e62c43ae46e7a46c8f473021f46
STARS1LVER <diegocardenaz11@gmail.com>
2024-01-31 13:24:39 -0500

    usando el defer views

--- src/app/shared/heavy-loaders-slow/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders-slow/heavy-loaders-slow.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template:`
-    <h1>Hola mundo</h1>
+    <h1 [ngClass]="['w-full h-[600px]', cssClass]"
+    >Hola mundo</h1>
   `
 })
 export class HeavyLoadersSlowComponent {
+
+  @Input({required: true}) public cssClass!: string;
+
+  constructor(){
+    const start = Date.now()
+    while(Date.now() - start < 3000){
+
+    }
+
+  }
 
 }
