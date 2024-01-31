637ca0007ca310a61829972aa414b4af2b813e08
STARS1LVER <diegocardenaz11@gmail.com>
2024-01-31 12:39:02 -0500

    usamos change detection - on push

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule,TitleComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './change-detection.component.html',
   styles: ``
 })
 export default class ChangeDetectionComponent {
+
+  public currentFramework = computed(
+    () => ` Change detection - ${this.frameworksSignal().name}`
+  )
+
+  public frameworksSignal = signal({
+    name: 'Angular',
+    release: 2016
+  })
+
+  public frameworksHasProperty = {
+    name: 'Angular',
+    release: 2016
+  };
+
+
+  constructor(){
+    setTimeout(()=>{
+
+      this.frameworksSignal.update(value => ({
+        ...value,
+        name: 'React'
+      }))
+
+    },3000)
+  }
 
 }
